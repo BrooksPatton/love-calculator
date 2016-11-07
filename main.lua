bbc2a023ac2607f2ee8660bb600b5ca8ba1db94a
Brooks Patton <brooks@brookzerker.com>
2016-11-06 21:25:32 -0700

    text is in the middle of the buttons now

--- main.lua
+++ main.lua
@@ -1,16 +1,16 @@
 function love.load()
-  one =   newButton({x = 0, y = 220}, {width = 50, height = 50})
-  two =   newButton({x = 60, y = 220}, {width = 50, height = 50})
-  three = newButton({x = 120, y = 220}, {width = 50, height = 50})
-  four =  newButton({x = 0, y = 160}, {width = 50, height = 50})
-  five =  newButton({x = 60, y = 160}, {width = 50, height = 50})
-  six =   newButton({x = 120, y = 160}, {width = 50, height = 50})
-  seven = newButton({x = 0, y = 100}, {width = 50, height = 50})
-  eight = newButton({x = 60, y = 100}, {width = 50, height = 50})
-  nine =  newButton({x = 120, y = 100}, {width = 50, height = 50})
-  zero =  newButton({x = 60, y = 280}, {width = 50, height = 50})
-  plus =  newButton({x = 0, y = 280}, {width = 50, height = 50}, {246,	135, 44})
-  equals =  newButton({x = 120, y = 280}, {width = 50, height = 50}, {246,	135, 44})
+  one =   newButton('1', {x = 0, y = 220}, {width = 50, height = 50})
+  two =   newButton('2', {x = 60, y = 220}, {width = 50, height = 50})
+  three = newButton('3', {x = 120, y = 220}, {width = 50, height = 50})
+  four =  newButton('4', {x = 0, y = 160}, {width = 50, height = 50})
+  five =  newButton('5', {x = 60, y = 160}, {width = 50, height = 50})
+  six =   newButton('6', {x = 120, y = 160}, {width = 50, height = 50})
+  seven = newButton('7', {x = 0, y = 100}, {width = 50, height = 50})
+  eight = newButton('8', {x = 60, y = 100}, {width = 50, height = 50})
+  nine =  newButton('9', {x = 120, y = 100}, {width = 50, height = 50})
+  zero =  newButton('0', {x = 60, y = 280}, {width = 50, height = 50})
+  plus =  newButton('+', {x = 0, y = 280}, {width = 50, height = 50}, {246,	135, 44})
+  equals =  newButton('=', {x = 120, y = 280}, {width = 50, height = 50}, {246,	135, 44})
 end
 
 function love.draw()
@@ -29,10 +29,10 @@ function love.draw()
 end
 
 function love.update(dt)
-  print('called: ', dt)
+
 end
 
-function newButton(location, size, color)
+function newButton(text, location, size, color)
   local self = {}
 
   self.color = color or {131, 192, 240}
@@ -41,9 +41,16 @@ function newButton(location, size, color)
   self.width = size.width
   self.height = size.height
 
+  local text = text
+
+  local font = love.graphics.getFont()
+
   self.draw = function()
     love.graphics.setColor(self.color[1], self.color[2], self.color[3])
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.printf(text, self.x, self.y + ((self.height / 2) - (font:getHeight() / 2)), self.width, 'center')
   end
 
   return self
