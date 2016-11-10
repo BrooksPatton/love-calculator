fb0ad976afc00f2246bf717bcfb76ee09355cdd2
Brooks Patton <brooks@brookzerker.com>
2016-11-09 21:46:47 -0700

    calculator is working

--- main.lua
+++ main.lua
@@ -1,46 +1,48 @@
 function love.load()
-  one =   newButton('1', {x = 0, y = 220}, {width = 50, height = 50})
-  two =   newButton('2', {x = 60, y = 220}, {width = 50, height = 50})
-  three = newButton('3', {x = 120, y = 220}, {width = 50, height = 50})
-  four =  newButton('4', {x = 0, y = 160}, {width = 50, height = 50})
-  five =  newButton('5', {x = 60, y = 160}, {width = 50, height = 50})
-  six =   newButton('6', {x = 120, y = 160}, {width = 50, height = 50})
-  seven = newButton('7', {x = 0, y = 100}, {width = 50, height = 50})
-  eight = newButton('8', {x = 60, y = 100}, {width = 50, height = 50})
-  nine =  newButton('9', {x = 120, y = 100}, {width = 50, height = 50})
-  zero =  newButton('0', {x = 60, y = 280}, {width = 50, height = 50})
-  plus =  newButton('+', {x = 0, y = 280}, {width = 50, height = 50}, {246,	135, 44})
-  equals =  newButton('=', {x = 120, y = 280}, {width = 50, height = 50}, {246,	135, 44})
+  buttons = {}
 
-  display = 0
+  buttons[1] =   newButton('1', {x = 0, y = 220}, {width = 50, height = 50})
+  buttons[2] =   newButton('2', {x = 60, y = 220}, {width = 50, height = 50})
+  buttons[3] = newButton('3', {x = 120, y = 220}, {width = 50, height = 50})
+  buttons[4] =  newButton('4', {x = 0, y = 160}, {width = 50, height = 50})
+  buttons[5] =  newButton('5', {x = 60, y = 160}, {width = 50, height = 50})
+  buttons[6] =   newButton('6', {x = 120, y = 160}, {width = 50, height = 50})
+  buttons[7] = newButton('7', {x = 0, y = 100}, {width = 50, height = 50})
+  buttons[8] = newButton('8', {x = 60, y = 100}, {width = 50, height = 50})
+  buttons[9] =  newButton('9', {x = 120, y = 100}, {width = 50, height = 50})
+  buttons[0] =  newButton('0', {x = 60, y = 280}, {width = 50, height = 50})
+  buttons.plus =  newButton('+', {x = 0, y = 280}, {width = 50, height = 50}, {246,	135, 44})
+  buttons.equals =  newButton('=', {x = 120, y = 280}, {width = 50, height = 50}, {246,	135, 44})
+
+  display = ''
   operator = nil
-  operandOne = nil
-  operandTwo = nil
+  operandOne = ''
+  operandTwo = ''
+  resetdisplay = true
+
+  maxDisplayLength = 5
 
   resultFont = love.graphics.newFont(50)
 end
 
 function love.draw()
-  one.draw()
-  two.draw()
-  three.draw()
-  four.draw()
-  five.draw()
-  six.draw()
-  seven.draw()
-  eight.draw()
-  nine.draw()
-  zero.draw()
-  plus.draw()
-  equals.draw()
+  for k,v in pairs(buttons) do
+    v.draw()
+  end
 
   love.graphics.setFont(resultFont)
   love.graphics.setColor(255, 255, 255)
   love.graphics.printf(display, 0, 25, 170, 'right')
 end
 
-function love.update(dt)
-
+function love.mousepressed(x, y, button)
+  if string.len(display) < maxDisplayLength then
+    for k,v in pairs(buttons) do
+      if v.clicked(x, y) then
+        updateCalculator(k)
+      end
+    end
+  end
 end
 
 function newButton(text, location, size, color)
@@ -64,5 +66,37 @@ function newButton(text, location, size, color)
     love.graphics.printf(text, self.x, self.y + ((self.height / 2) - (font:getHeight() / 2)), self.width, 'center')
   end
 
+  self.clicked = function(x, y)
+    return (x > self.x) and (x < self.x + self.width) and (y > self.y) and (y < self.y + self.height)
+  end
+
   return self
+end
+
+function updateCalculator(buttonKey)
+  if not operator and (buttonKey ~= 'plus' and buttonKey ~= 'equals') then
+    if resetdisplay then
+      display = ''
+      resetdisplay = false
+    end
+
+    display = display .. buttonKey
+    operandOne = operandOne .. buttonKey
+  elseif (buttonKey ~= 'plus' and buttonKey ~= 'equals') then
+    if resetdisplay then
+      display = ''
+      resetdisplay = false
+    end
+    
+    display = display .. buttonKey
+    operandTwo = operandTwo .. buttonKey
+  elseif buttonKey == 'plus' then
+    operator = 'plus'
+    resetdisplay = true
+  else
+    display = tonumber(operandOne) + tonumber(operandTwo)
+    operandOne, operandTwo = '', ''
+    operator = nil
+    resetdisplay = true
+  end
 end
